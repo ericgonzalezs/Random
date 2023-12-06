e7fb44f3018d1e2d36114f80d1afffd2b392d264
ericgonzalezs <shinken123@gmail.com>
2023-12-06 13:48:29 -0800

    Update CalculateRR_per_Chr.R

--- CalculateRR_per_Chr.R
+++ CalculateRR_per_Chr.R
@@ -1,7 +1,10 @@
+#this table has genetic distances and physical positions
 Pos <- read.table("Table_For_Marey_names.txt", header=T)
+
+#this table has values increasing every 500bp, from 0 to the legth of each chromosome 
 Pos_500kb <- read.table("Table_with_pos_to_getRR_500kb.txt", head =T)
 
-
+#to loop over all chromosomes
 #for(chr in unique(Pos$map)) {
 for(chr in c("HanXRQChr01", "HanXRQChr02", "HanXRQChr08", "HanXRQChr09")) {
 
@@ -24,19 +27,18 @@ while (any(diff(Pos2_R_F1$gen) < 0)) {
 
 spline2 <- smooth.spline(Pos2_R_F1$phys, Pos2_R_F1$gen, spar = 0.05)
 Pos2_R_F1_R2 <- cbind(Pos2_R_F1, R2=residuals(spline2))
-
 Pos2_R_F1_R2_F2 <- subset(Pos2_R_F1_R2, Pos2_R_F1_R2$R2 > -0.1 & Pos2_R_F1_R2$R2 < 0.1)
 
-#creating the curve I'll use to report RR
+#creating the cubic spline I'll use to report RR
 spline3 <- smooth.spline(Pos2_R_F1_R2_F2$phys, Pos2_R_F1_R2_F2$gen, spar = 0.05)
 
 #file with pos every 500kb
 Pos_500kb_2 <- subset(Pos_500kb, Pos_500kb$map==chr)
 
-#obtaining the first derivative
+#obtaining the first derivative (RR)
 X_derivatives_500kb <- predict(spline3, Pos_500kb_2$phys, deriv = 1)$y
 
-#converting bp to Mb
+#converting cm/bp to cm/Mb
 Pos_500kb_2_der <- cbind(Pos_500kb_2, RR=X_derivatives_500kb * 1e6)
 
 #setting to 0 negative values
